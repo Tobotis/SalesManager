d1b461e0a0f5f71600a480d4b42d65abcdacc9e5
Tobias <to.steinbrecher@gmail.com>
2022-05-16 17:23:19 +0200

    added firebase config data

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAPsdSU85dH3wnAHpWB719kMHE7_w2f-JI",
+  authDomain: "finanz-komitee.firebaseapp.com",
+  projectId: "finanz-komitee",
+  storageBucket: "finanz-komitee.appspot.com",
+  messagingSenderId: "402888676864",
+  appId: "1:402888676864:web:e6735b6e6357d4d0b96ae5",
+  measurementId: "G-EEH7GPCR18",
 });
 
 export const auth = getAuth(app);
